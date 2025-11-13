4c0dd729a47dc8c4e99f376b7f6526d4d4d15d88
Jordan Valente <jordanvalente14@gmail.com>
2025-11-13 16:31:16 +0100

    d
    d

---
+++ gestion_factures.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 13 nov. 2025 à 16:28
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `gestion_factures`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `clients`
+--
+-- Erreur de lecture de structure pour la table gestion_factures.clients : #1932 - Table 'gestion_factures.clients' doesn't exist in engine
+-- Erreur de lecture des données pour la table gestion_factures.clients : #1064 - Erreur de syntaxe près de 'FROM `gestion_factures`.`clients`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `factures`
+--
+-- Erreur de lecture de structure pour la table gestion_factures.factures : #1932 - Table 'gestion_factures.factures' doesn't exist in engine
+-- Erreur de lecture des données pour la table gestion_factures.factures : #1064 - Erreur de syntaxe près de 'FROM `gestion_factures`.`factures`' à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
